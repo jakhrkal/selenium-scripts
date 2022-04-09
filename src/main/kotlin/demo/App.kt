b576eca1f22caaef10cf0692b5b9d7a5ac4a22c2
Jakub Hrkal <jakhrkal@gmail.com>
2022-04-09 20:37:56 +0000

    Browser drivers experiments

--- src/main/kotlin/demo/App.kt
+++ src/main/kotlin/demo/App.kt
@@ -1,5 +1,14 @@
 package demo
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 class App {
     val greeting: String
         get() {
@@ -8,5 +17,10 @@ class App {
 }
 
 fun main() {
-    println(App().greeting)
+    val caps = DesiredCapabilities();
+    caps.setJavascriptEnabled(true); // not really needed: JS enabled by default
+    caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "src/test/resources/phantomjs");
+
+    val driver = PhantomJSDriver(caps);
+    driver.get("https://devqa.io");
 }
