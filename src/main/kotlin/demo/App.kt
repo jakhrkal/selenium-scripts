051f74fef540fa05c922ba9fbc96f789094737a2
Jakub Hrkal <jakhrkal@gmail.com>
2022-09-13 21:11:57 +0000

    Dockerfile - WIP

--- src/main/kotlin/demo/App.kt
+++ src/main/kotlin/demo/App.kt
@@ -1,21 +1,10 @@
 package demo
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import java.io.File
-import java.sql.Timestamp
 import java.time.format.DateTimeFormatter
 import java.time.Instant
-
-class App {
-}
 
 fun main() {
     val wdm = WebDriverManager.chromedriver().browserInDocker().enableVnc();
