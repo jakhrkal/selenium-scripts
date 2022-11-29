274e580e9bb6e3b7af68bdeb80ad90a229462617
Jakub Hrkal <jakhrkal@gmail.com>
2022-11-29 09:58:39 +0000

    docker-compose, chrome-standalone img

--- src/main/kotlin/demo/App.kt
+++ src/main/kotlin/demo/App.kt
@@ -1,17 +1,18 @@
 package demo
 
-import org.openqa.selenium.By;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By
+import io.github.bonigarcia.wdm.WebDriverManager
 import java.io.File
 import java.time.format.DateTimeFormatter
 import java.time.Instant
 
 fun main() {
-    val wdm = WebDriverManager.chromedriver().browserInDocker().enableVnc();
+    val wdm = WebDriverManager.chromedriver().remoteAddress("http://chrome:4444/wd/hub");
     val driver = wdm.create();
     driver.get("https://example.org/");
     val heading1 = driver.findElement(By.tagName("h1"));
     println("Heading: " + heading1.text);
+    // todo pull file out of Docker container
     val resultFile = File("result-" + DateTimeFormatter.ISO_INSTANT.format(Instant.now()) + ".txt")
     resultFile.appendText("Result:\n");
     resultFile.appendText(heading1.text);
