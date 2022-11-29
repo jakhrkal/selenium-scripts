ad1192f4cfa51c6a6c63ebdd1eca0d12bdd39482
Jakub Hrkal <jakhrkal@gmail.com>
2022-11-29 12:14:12 +0000

    volume mapping for result file

--- src/main/kotlin/demo/App.kt
+++ src/main/kotlin/demo/App.kt
@@ -3,6 +3,8 @@ package demo
 import org.openqa.selenium.By
 import io.github.bonigarcia.wdm.WebDriverManager
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.time.format.DateTimeFormatter
 import java.time.Instant
 
@@ -12,8 +14,8 @@ fun main() {
     driver.get("https://example.org/");
     val heading1 = driver.findElement(By.tagName("h1"));
     println("Heading: " + heading1.text);
-    // todo pull file out of Docker container
-    val resultFile = File("result-" + DateTimeFormatter.ISO_INSTANT.format(Instant.now()) + ".txt")
+    Files.createDirectories(Paths.get("output"))
+    val resultFile = File("output/result-" + DateTimeFormatter.ISO_INSTANT.format(Instant.now()) + ".txt")
     resultFile.appendText("Result:\n");
     resultFile.appendText(heading1.text);
     wdm.quit();
